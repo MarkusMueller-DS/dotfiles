eeb74777b72d356663eaac90e8cd76d1d6aeb76e
Markus Müller <markus.j.mueller94@icloud.com>
2021-12-21 21:15:52 +0100

    on the way to full Lua Neovim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,10 @@
-require('user.options') 
+require('user.options')
 require('user.keymaps')
 require('user.plugins')
 require('user.colorscheme')
 require('user.cmp')
+require('user.toggleterm')
+require('user.lsp')
 
 vim.g.python3_host_prog = '/Users/markusmuller/miniforge3/bin/python3'
 
